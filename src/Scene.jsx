446a153d97e1906214ded2cb4188a2361a084b80
melissa banoen garde <m.banoengarde@gmail.com>
2023-02-23 01:19:43 -0500

    Add torusknot to the scene

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useRef } from "react";
+import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 const Scene = () => {
+  const torusRef = useRef();
+
+  // useFrame((state, delta) => {
+  //   torusRef.current.rotation.z += delta / 3;
+  // });
+
   return (
-    <mesh>
-      <coneGeometry />
-      <meshStandardMaterial color="red" wireframe />
-    </mesh>
+    <>
+      <OrbitControls />
+      <ambientLight />
+      <pointLight position={[-1, 1, 1]} intensity={1} />
+
+      <mesh scale={0.09} ref={torusRef} rotation={[0, 0.25, 0]}>
+        <torusKnotGeometry args={[15, 1.2, 600, 12, 2, 18]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </>
   );
 };
 
